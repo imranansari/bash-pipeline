238738b122dc897165434217216fd1ec95f8d570
Imran Ansari <iansari@apple.com>
2017-06-29 13:44:33 -0700

    clone

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+BASEDIR=$(dirname $0)
+
 function build(){
   echo "hello ..."
 }
 
 function checkout(){
     echo "checkout ..."
+    mkdir workspace
+    echo ${BASEDIR}/workspace
+    (cd workspace &&  git clone git@github.com:imranansari/bash-pipeline.git)
+    #${BASEDIR}/tmp_workspace git clone git@github.com:imranansari/bash-pipeline.git
 }
 
 function pipeline(){
